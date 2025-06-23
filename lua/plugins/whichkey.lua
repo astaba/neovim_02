ea44c704bd8299d6c986ca3859d3d40780e0f373
Astar <bahouidi99@gmail.com>
2025-06-23 07:03:22 +0100

    Install which-key

---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,30 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    local wk = require("which-key")
+
+    wk.setup({
+      triggers = {
+        { "<Leader>", mode = { "n", "v" } },
+      },
+      preset = "modern",
+      show_keys = true,
+    })
+
+    wk.add({
+      {
+        "<leader>b",
+        group = "[B]uffers",
+        expand = function() return require("which-key/extras").expand.buf() end
+      },
+    }) -- end of wk.add()
+  end,
+  keys = {
+    {
+      "<leader>?",
+      function() require("which-key").show({ global = false }) end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
