e892859a4d51a5186841f733e99ce2834d4eb186
Astar <bahouidi99@gmail.com>
2025-06-23 07:10:52 +0100

    Install and test conform.nvim formatter

---
+++ lua/plugins/hello.js
@@ -0,0 +1,11 @@
+// function greattings(who) {
+//   console.log(`How is it going ${who}? Whish you well!`);
+// }
+//
+// greattings('Youta');
+
+function greattings(who) {
+    console.log(`How is it going ${who}? Whish you well!`);
+}
+
+greattings("Youta");
