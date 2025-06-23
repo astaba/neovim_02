3b6058095790f3ba1c86a96c30effee14076d2f0
Astar <bahouidi99@gmail.com>
2025-06-23 07:39:13 +0100

    Test blinkcmp fast completion

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -16,4 +16,34 @@ return {
       }
     }
   },
+  -- tailwindcss-colorizer-cmp
+  { "roobert/tailwindcss-colorizer-cmp.nvim", enabled = false, },
+  -- colorizer
+  {
+    "NvChad/nvim-colorizer.lua",
+    enabled = false,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {},
+    config = function()
+      local nvchadcolorizer = require("colorizer")
+      local tailwindcolorizer = require("tailwindcss-colorizer-cmp")
+
+      nvchadcolorizer.setup({
+        user_default_options = {
+          tailwind = true,
+        },
+        filetypes = { "html", "css", "javascript", "typescript", "jsx", "tsx", "vue", "svelte" },
+      })
+
+      tailwindcolorizer.setup({
+        color_square_width = 2,
+      })
+
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+        callback = function()
+          vim.cmd("ColorizerAttachToBuffer")
+        end,
+      })
+    end,
+  },
 }
